76fb0be8dde19d4281ff0c58092c6f58d8d02bdb
Tyson Gern <tyson@initialcapacity.io>
2024-04-30 08:31:00 -0600

    Add client completion test

--- pkg/ai/client_test.go
+++ pkg/ai/client_test.go
@@ -12,15 +12,6 @@ import (
 	"testing"
 )
 
-func testAiClient(openAiEndpoint string) ai.Client {
-	openAiClient, err := azopenai.NewClientForOpenAI(openAiEndpoint, nil, nil)
-	if err != nil {
-		log.Fatal(fmt.Errorf("unable to create Open AI client: %w", err))
-	}
-
-	return ai.Client{OpenAiClient: openAiClient}
-}
-
 func TestClient_CreateEmbedding(t *testing.T) {
 	endpoint, server := testsupport.StartTestServer(t, func(mux *http.ServeMux) {
 		mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
@@ -41,4 +32,35 @@ func TestClient_CreateEmbedding(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, []float32{1, 2, 3, 4}, result)
+}
+
+func TestClient_GetChatCompletion(t *testing.T) {
+	endpoint, server := testsupport.StartTestServer(t, func(mux *http.ServeMux) {
+		mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
+		mux.HandleFunc("/chat/completions", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write(data(`{ "choices": [ { "delta": { "role": "assistant", "content": "Sounds good" } } ] }`))
+			assert.NoError(t, err)
+		})
+	})
+	defer testsupport.StopTestServer(t, server)
+
+	client := testAiClient(endpoint)
+	completion, err := client.GetChatCompletion(context.Background(), []ai.ChatMessage{})
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Sounds good", <-completion)
+}
+
+func data(json string) []byte {
+	return []byte(fmt.Sprintf("data:%s\ndata:[DONE]", json))
+}
+
+func testAiClient(openAiEndpoint string) ai.Client {
+	openAiClient, err := azopenai.NewClientForOpenAI(openAiEndpoint, nil, nil)
+	if err != nil {
+		log.Fatal(fmt.Errorf("unable to create Open AI client: %w", err))
+	}
+
+	return ai.Client{OpenAiClient: openAiClient}
 }
