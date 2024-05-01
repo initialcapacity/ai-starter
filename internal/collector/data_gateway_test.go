b1e2979bf09185f884d7611dc895ce473a9a43db
Tyson Gern <tyson@initialcapacity.io>
2024-05-01 05:58:01 -0600

    Add data gateway test

---
+++ internal/collector/data_gateway_test.go
@@ -0,0 +1,47 @@
+package collector_test
+
+import (
+	"database/sql"
+	"github.com/initialcapacity/ai-starter/internal/collector"
+	"github.com/initialcapacity/ai-starter/pkg/dbsupport"
+	"github.com/initialcapacity/ai-starter/pkg/testsupport"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDataGateway_Save(t *testing.T) {
+	testDb := testsupport.NewTestDb(t)
+	defer testDb.Close()
+	testDb.ClearTables("chunks", "data")
+
+	dataGateway := collector.NewDataGateway(testDb.DB)
+
+	id, err := dataGateway.Save("https://example.com", "some content")
+	assert.NoError(t, err)
+
+	savedContent, err := dbsupport.QueryOne(testDb.DB, "select content from data where id = $1", func(row *sql.Row, content *string) error {
+		return row.Scan(content)
+	}, id)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "some content", savedContent)
+}
+
+func TestDataGateway_Exists(t *testing.T) {
+	testDb := testsupport.NewTestDb(t)
+	defer testDb.Close()
+	testDb.ClearTables("chunks", "data")
+
+	dataGateway := collector.NewDataGateway(testDb.DB)
+
+	_, err := dataGateway.Save("https://example.com", "some content")
+	assert.NoError(t, err)
+
+	shouldExist, err := dataGateway.Exists("https://example.com")
+	assert.NoError(t, err)
+	assert.True(t, shouldExist)
+
+	shouldNotExist, err := dataGateway.Exists("https://not-there.example.com")
+	assert.NoError(t, err)
+	assert.False(t, shouldNotExist)
+}
