d713dd241cd9ae36e2006b542466d72dcc44918c
Tyson Gern <tyson@initialcapacity.io>
2024-05-06 06:01:39 -0600

    Create a unique database for each test
    This should fix errors in parallel test runs

--- pkg/testsupport/test_connection.go
+++ pkg/testsupport/test_connection.go
@@ -2,23 +2,44 @@ package testsupport
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/initialcapacity/ai-starter/pkg/dbsupport"
 	"github.com/stretchr/testify/assert"
+	"math/rand/v2"
 	"testing"
 )
 
 type TestDb struct {
-	DB *sql.DB
-	t  *testing.T
+	DB         *sql.DB
+	t          *testing.T
+	testDbName string
 }
 
 func NewTestDb(t *testing.T) *TestDb {
-	return &TestDb{DB: dbsupport.CreateConnection("postgres://starter:starter@localhost:5432/starter_test?sslmode=disable"), t: t}
+	testDbName := fmt.Sprintf("starter_test_%d", rand.IntN(1_000_000))
+	withSuperDb(t, func(superDb *sql.DB) {
+
+		_, err := superDb.Exec(fmt.Sprintf("create database %s template starter_test", testDbName))
+		assert.NoError(t, err, "unable to create test database")
+		_, err = superDb.Exec(fmt.Sprintf("grant all privileges on database %s to starter", testDbName))
+		assert.NoError(t, err, "unable to grant permissions for test database")
+	})
+
+	return &TestDb{
+		DB:         dbsupport.CreateConnection(fmt.Sprintf("postgres://starter:starter@localhost:5432/%s?sslmode=disable", testDbName)),
+		t:          t,
+		testDbName: testDbName,
+	}
 }
 
 func (tdb *TestDb) Close() {
 	err := tdb.DB.Close()
 	assert.NoError(tdb.t, err)
+
+	withSuperDb(tdb.t, func(superDb *sql.DB) {
+		_, err = superDb.Exec(fmt.Sprintf("drop database %s", tdb.testDbName))
+		assert.NoError(tdb.t, err, "unable to drop test database")
+	})
 }
 
 func (tdb *TestDb) ClearTables() {
@@ -30,4 +51,14 @@ func (tdb *TestDb) ClearTables() {
 func (tdb *TestDb) Execute(statement string, arguments ...any) {
 	_, err := tdb.DB.Exec(statement, arguments...)
 	assert.NoError(tdb.t, err)
+}
+
+func withSuperDb(t *testing.T, action func(superDb *sql.DB)) {
+	superDb := dbsupport.CreateConnection("postgres://super_test@localhost:5432/postgres?sslmode=disable")
+	defer func(superDb *sql.DB) {
+		err := superDb.Close()
+		assert.NoError(t, err, "unable to close connection to postgres")
+	}(superDb)
+
+	action(superDb)
 }
